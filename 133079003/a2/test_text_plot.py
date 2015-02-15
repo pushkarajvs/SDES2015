75a9e26b923b85ce2f648f2d3172fc2a760f3a91
pushkarajvs <pushkarajvs@ee.iitb.ac.in>
2015-02-16 04:52:10 +0530

    Added a function to plot a matrix and its test.
    The function only plots a * wherever the matrix has a value 1.

--- 133079003/a2/test_text_plot.py
+++ 133079003/a2/test_text_plot.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
+import text_plot as tp
 import math
 
 def test_zeros():
-    assert zeros(5)==[0,0,0,0,0], "Not working for size=5"
-    assert zeros(1)==[0], "Not working for size=1"
+    assert tp.zeros(5)==[0,0,0,0,0], "Not working for size=5"
+    assert tp.zeros(1)==[0], "Not working for size=1"
     
+def test_plot_matrix():
+    rows=30
+    columns=40
+    matrix=tp.return_matrix(rows,columns)
+    matrix[2][3]=1
+    matrix[29][0]=1
+    plot=tp.plot_matrix(matrix,1)
+    for i in range(0,rows):
+        for j in range(0,columns):
+            assert ((matrix[i][j]==0 and plot[i*columns+j]==" ") or (matrix[i][j]==1 and plot[i*columns+j]=="*")), "Plot not correct"
+          
+          
+if(__name__=="__main__"):
+    test_zeros()
+    test_plot_matrix()
--- 133079003/a2/text_plot.py
+++ 133079003/a2/text_plot.py
@@ -15,3 +15,25 @@ def return_matrix(rows,columns):
     for i in range(0,rows):
         matrix.append(zeros(columns))
     return matrix
+    
+def plot_matrix(matrix,test=0):
+    rows=len(matrix)
+    columns=len(matrix[0])
+    if(test==1):
+        test_build=""
+    for i in range(0,rows):
+        build=""
+        for j in range(0,columns):
+            if(matrix[i][j]):
+                build=build+"*"
+            else:
+                build=build+" "
+        if(test==0):
+            print build
+        elif(test==1):
+            test_build=test_build+build
+    if(test==1):
+        return(test_build)
+        
+    
+
