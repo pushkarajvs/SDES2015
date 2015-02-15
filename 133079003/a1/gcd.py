4ab1ceb690594e36106147632bd95f8d5b8fedf0
pushkaraj <pushkarajvs@gmail.com>
2015-02-16 03:27:41 +0530

    Added docstring for gcd function and "raise statements" for some errors.
    TypeError for inputs not being int or long and
    ValueError for non-positive inputs.

--- 133079003/a1/gcd.py
+++ 133079003/a1/gcd.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 "Contains a function to calculate GCD (Greatest Common Divisor) of two numbers"
 def gcd(a, b):
+
+    """
+         Calculates GCD of two numbers
+         Syntax - gcd(a, b)
+         Both a and b should be either int or long.
+    """
+    
+    if(type(a) != int or type (b) != int):
+        raise TypeError("Both values should be either an int or a long.")
+
+    if(a <= 0 or b <= 0):
+        raise ValueError("Both values should be positive.")
+    
     while b:
         a, b = b, a % b
     return a
