b6b59df30f608c88dfb224ae27b14220d9260d16
pushkaraj <pushkarajvs@gmail.com>
2015-02-16 03:41:57 +0530

    Added tests for checking raising of errors.
    Made the test print "all good", if all test are passed and "At least one test
    failed" otherwise.

--- 133079003/a1/test_gcd.py
+++ 133079003/a1/test_gcd.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 import gcd
 def test_gcd():
+    flag=0
     assert gcd.gcd(2,40)==2, "Function doesn't give GCD of 2 and 40 as 2"
     assert gcd.gcd(19,35)==1, "Function doesn't give GCD of 19 and 35 as 1"
-    
+    try:
+        trash=gcd.gcd(2,-700)
+        print "Failed to raise ValueError for negative input"
+        flag=1
+    except ValueError:
+        pass
+    try:
+        trash=gcd.gcd(2,700.3)
+        print "Failed to raise TypeError for float value"
+        flag=1
+    except TypeError:
+        pass
+    if(flag==0):
+        print "All good"
+    else:
+        print "At least one test failed"
+        
 if(__name__=="__main__"):
     test_gcd()
